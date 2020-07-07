2c536771f966ea7f613c7cc1f7adcf5e49df6bb9
Shubham <sbhtwr170@gmail.com>
2020-07-07 23:34:53 +0530

    Updated NN

--- nn.py
+++ nn.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
 
+# threshold probability for cell being ON
+thres = 0.01
 
 # load the dataset
 df_train = np.load('ppdataset_train.npy', allow_pickle='TRUE').item()
@@ -27,13 +29,8 @@ print('Output size: {}'.format(output_size))
 # num rows 
 num_rows = len(df_train['y'])
 
-# size of hidden layer
-hidden_layer_size = int((input_size + output_size)/2)
-
-print('Size of hidden layer: {}'.format(hidden_layer_size))
-
-# A neural network with a single hidden layer. 
-clf = MLPClassifier(solver='sgd', hidden_layer_sizes=(hidden_layer_size), random_state=1, verbose=True)
+### preprocess train data ###
+print('Pre-process train data...')
 
 nans = 0
 rows_to_del = []
@@ -61,16 +58,51 @@ for i in range(0, num_rows):
 		print('count: {} i: {}'.format(nans, i))
 		rows_to_del.append(i)
 
-		
+df_train['y'] = (df_train['y'] > thres).astype(int)
+### preprocess test data ###
+print('Pre-process test data...')
+num_rows_test = len(df_test['y'])
+
+nans = 0
+rows_to_del = []
+# print the dataset 
+for i in range(0, num_rows_test):
+	if (np.isnan(df_test['X'].iloc[i,:]).any() or np.isnan(df_test['y'].iloc[i,:]).any()):
+		nans += 1
+		print('count: {} i: {}'.format(nans, i))
+		rows_to_del.append(i)
+
+
+df_test['X'].drop(df_test['X'].index[rows_to_del], inplace=True)	
+df_test['y'].drop(df_test['y'].index[rows_to_del], inplace=True)	
+print('Deleted {} rows containing NaN values'.format(nans))		
+
+nans = 0
+rows_to_del = []
+# print the dataset 
+for i in range(0, num_rows_test):
+	if (np.isnan(df_test['X'].iloc[i,:]).any() or np.isnan(df_test['y'].iloc[i,:]).any()):
+		nans += 1
+		print('count: {} i: {}'.format(nans, i))
+		rows_to_del.append(i)
+
+df_test['y'] = (df_test['y'] > thres).astype(int)		
 #arr = np.array(df_train['y'])
 #pr_mean = arr.mean()
 #print(pr_mean)
 #print(df_train['y'].mean())
 
-df_train['y'] = (df_train['y'] > 0.01).astype(int)
+
 #print(df_train['y'])
 
+# size of hidden layer
+hidden_layer_size = int((input_size + output_size)/2)
+
+print('Size of hidden layer: {}'.format(hidden_layer_size))
+
+# A neural network with a single hidden layer. 
+clf = MLPClassifier(solver='sgd', hidden_layer_sizes=(hidden_layer_size), random_state=1, verbose=True)
 #Train the neural network with the training data
-clf.fit(df_train['X'], df_train['y'])
+#clf.fit(df_train['X'], df_train['y'])
 
 
